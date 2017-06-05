4022cde963cb46768a3aa0976365ae2046ca3c01
gpdoud <gpdoud@gmail.com>
2017-06-05 16:20:36 -0400

    initial commit

---
+++ BowlingProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BowlingProject {
+
+    public class Program {
+
+        //int[] scores = new int[10];
+        private List<int> scores = new List<int>();
+
+        void Run() {
+            Random rnd = new Random();
+            for(int idx = 0; idx < 10; idx++) {
+                int number = rnd.Next(11);
+                scores.Add(number);
+            }
+            foreach(int anInt in scores) {
+                Debug.WriteLine(anInt);
+            }
+        }
+
+        static void Main(string[] args) {
+            (new Program()).Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bowling Solution
+
+Create a single game of bowling using generic arrays and random integers then total up the score.
